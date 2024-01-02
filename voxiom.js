dcc2038636d4f5e40e562d67a65e3e814c72b951
Rob4ig <136381220+Rob4ig@users.noreply.github.com>
2024-01-02 21:07:55 +0200

    Create voxiom.js

---
+++ voxiom.js
@@ -0,0 +1,11 @@
+class Voxiom {
+    constructor(t, c, a) {
+        this.element = document.createElement(t)
+        this.element.className = c
+        a.appendChild(this.element)
+    }
+    text(i) { this.element.innerHTML = i; return this }
+    attr(n, v) { this.element[n] = v; return this }
+    style(n, v) { this.element.style[n] = v; return this }
+    append(...cc) { cc.forEach(c => this.element.appendChild(c.element)) }
+}
