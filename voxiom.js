bd9753ab5e295e1caac36dcaa24f7b76b759f742
Rob4ig <136381220+Rob4ig@users.noreply.github.com>
2024-01-03 16:31:50 +0200

    Update voxiom.js

--- voxiom.js
+++ voxiom.js
@@ -9,19 +9,3 @@ class Voxiom {
     style(n, v) { this.element.style[n] = v; return this }
     append(...cc) { cc.forEach(c => this.element.appendChild(c.element)) }
 }
-const Inv = {
-    "F6J": "Combat Assault Rifle",
-    "FyR": "Tactical Assault Rifle",
-    "IABXQl": "Surge Assault Rifle",
-    "FDt": "Elite Assault Rifle",
-    "E+J": "Burst Shotgun",
-    "GXV": "Light Submachine Gun",
-    "H9F": "Compact Submachine Gun",
-    "Epx": "Light Sniper Rifle",
-    "FuN": "Heavy Sniper Rifle",
-    "Ehd": "Strike Pistol",
-    "F6R": "Magnum Pistol",
-    "C2d": "Shovel"
-}
-const a = "a"
-let b = "b"
