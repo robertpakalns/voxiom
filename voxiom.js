86198cdae3c5c35f3ae899bb4622cf69d5049836
Rob4ig <136381220+Rob4ig@users.noreply.github.com>
2024-01-03 16:22:14 +0200

    Update voxiom.js

--- voxiom.js
+++ voxiom.js
@@ -1,29 +1,15 @@
-// voxiom.js
-
 class Voxiom {
     constructor(t, c, a) {
-        this.element = document.createElement(t);
-        this.element.className = c;
-        a.appendChild(this.element);
+        this.element = document.createElement(t)
+        this.element.className = c
+        a.appendChild(this.element)
     }
-    text(i) {
-        this.element.innerHTML = i;
-        return this;
-    }
-    attr(n, v) {
-        this.element[n] = v;
-        return this;
-    }
-    style(n, v) {
-        this.element.style[n] = v;
-        return this;
-    }
-    append(...cc) {
-        cc.forEach(c => this.element.appendChild(c.element));
-    }
+    text(i) { this.element.innerHTML = i; return this }
+    attr(n, v) { this.element[n] = v; return this }
+    style(n, v) { this.element.style[n] = v; return this }
+    append(...cc) { cc.forEach(c => this.element.appendChild(c.element)) }
 }
-
-const inventory = {
+const Inv = {
     "F6J": "Combat Assault Rifle",
     "FyR": "Tactical Assault Rifle",
     "IABXQl": "Surge Assault Rifle",
@@ -36,6 +22,4 @@ const inventory = {
     "Ehd": "Strike Pistol",
     "F6R": "Magnum Pistol",
     "C2d": "Shovel"
-};
-
-export { Voxiom, inventory };
+}
